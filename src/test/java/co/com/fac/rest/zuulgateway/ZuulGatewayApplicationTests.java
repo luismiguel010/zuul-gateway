597f724dc7a40aa969a1eeca4dac0abbe4fc92cc
Luis  Montes Zuluaga <luismonteszuluaga@MacBook-Air-de-Luis.local>
2020-01-31 16:13:26 -0500

    first commit

---
+++ src/test/java/co/com/fac/rest/zuulgateway/ZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.fac.rest.zuulgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
